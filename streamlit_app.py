7d363b6b37716a808965a50b2786f52be5bef305
MichaelStuart335 <129512725+MichaelStuart335@users.noreply.github.com>
2023-04-04 12:27:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,5 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
